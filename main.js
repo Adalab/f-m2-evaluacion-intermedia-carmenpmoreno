24ba0a97a19d5dbe55a29f985fc40af815150f5e
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-08 18:32:17 +0200

    start function js

--- main.js
+++ main.js
@@ -9,11 +9,45 @@ console.log('Hola');
 
 // Programación del juego:
 
-// FUNCIÓN principal: Al pulsar el botón: 
+// CONSTANTES NECESARIAS
+
+    // botón
+    const button = document.querySelector('.button');
+    // input
+    const input = document.getElementById('number');
+    // feedback
+    const feedback = document.querySelector('.feedback');
+    // contador
+    const counter = document.querySelector('.counter');
+    
+    console.log(button, input, feedback, counter);
+
+
+// Todo sucede al Al pulsar el botón: Listener sobre el botón
+button.addEventListener('click', getInputandCompare)
+
+// Función handler:
+function getInputandCompare () {
     // se accede al input.value y se muestra en consola
-    // se genera un número al azar
+    const inputNumber = input.value;
+    console.log(inputNumber);
+    // se genera un número al azar y se muestra en la consola
+    const randomNumber = getRandomNumber(100);
+    console.log(randomNumber);
     // se compara ese número con el del input = feedback en función de si es más alto/bajo/igual
+    if ((parseInt(inputNumber)) >= randomNumber) {
+        const feedbackValue = feedback.innerHTML;
+        feedbackValue = 'demasiado alto';
+        console.log(funcionaaa);
+    }
     // tras cada intento, se cuenta 1 en el contador
 
-// FUNCIONES de apoyo:
-    // contador de números aleatorios
+}
+    
+// Función de apoyo:contador de números aleatorios
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+  
+  // píntame en la consola un número random hasta 100;
+//   console.log('> ' + getRandomNumber(100));
