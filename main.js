47530dce55d3a0f06177c11094fa48d8413d2499
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-08 17:49:24 +0200

    first html and main structure

---
+++ main.js
@@ -0,0 +1,19 @@
+'user strict';
+
+console.log('Hola');
+
+// Instrucciones juego:
+//  - Juego en el que puedes introducir un número en un input y te lo compara con un número al azar del 1 al 100.
+//  - Da un feedback: número introducido es demasiado alto, bajo o es el acertado
+//  - Marca el número de intentos
+
+// Programación del juego:
+
+// FUNCIÓN principal: Al pulsar el botón: 
+    // se accede al input.value y se muestra en consola
+    // se genera un número al azar
+    // se compara ese número con el del input = feedback en función de si es más alto/bajo/igual
+    // tras cada intento, se cuenta 1 en el contador
+
+// FUNCIONES de apoyo:
+    // contador de números aleatorios
