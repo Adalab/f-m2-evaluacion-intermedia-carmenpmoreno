0ad9d9fe300543ff27d34f26e9e1024c22d22099
carmenpmoreno <carmenp.moreno@gmail.com>
2019-04-16 13:38:01 +0200

    organice folders and unify callback functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,13 +16,24 @@ const input = document.getElementById('number');
 const feedback = document.querySelector('.feedback');
 // contador
 let counter = document.querySelector('.counter');
-console.log(button, input, feedback, counter);
 
+// PONER EN MARCHA EL CONTADOR DE INTENTOS: dar un valor inicial "0" al contador
+counter.innerHTML = 0;
+
+// FUNCION DE APOYO:contador de números aleatorios
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+  }
+// se genera un número al azar y se muestra en la consola
+const randomNumber = getRandomNumber(100);
+console.log(randomNumber);
+
+// PARA DAR FEEDBACK Y REGISTRAR LOS INTENTOS CON EL CONTADOR
 //Listener sobre el botón
-button.addEventListener('click', getInputandCompare)
+button.addEventListener('click', compareInputAndCount)
 
-// Función handler:
-function getInputandCompare () {
+// Función handler: 
+function compareInputAndCount () {
     // se accede al input.value y se muestra en consola
     const inputNumber = parseInt(input.value);
     console.log(inputNumber);
@@ -36,23 +47,9 @@ function getInputandCompare () {
     else if (inputNumber === randomNumber) {
         feedback.innerHTML = '¡LO HAS CONSEGUIDO!';
     }
+    // se añaden "1" al marcador, con cada nuevo intento
+        counter.innerHTML = parseInt(counter.innerHTML) + 1;
 }
-// Función de apoyo:contador de números aleatorios
-function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-  }
-// se genera un número al azar y se muestra en la consola
-const randomNumber = getRandomNumber(100);
-console.log(randomNumber);
 
-// PARA PONER EN MARCA EL CONTADOR DE INTENTOS
-// dar un valor inicial "0" al contador
-counter.innerHTML = 0;
-// listener para que cada vez que des al botón cuente uno más en el contador
-button.addEventListener('click', counterInputs)
-// función para que cada vez que se de al botón cuente 1
-function counterInputs() {
-    counter.innerHTML = parseInt(counter.innerHTML) + parseInt(1);
-}
     
 
